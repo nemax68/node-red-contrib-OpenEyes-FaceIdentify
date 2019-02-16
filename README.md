42b781be1f24a9a321369b806a9389db5db9fe24
Massimiliano Negretti <max.negretti@tiscali.it>
2019-02-16 21:56:28 +0100

    Add files via upload
    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Description
+===========
+
+![OpenEyes-FaceIdentify-flow](https://github.com/nemax68/node-red-contrib-OpenEyes-FaceIdentify/blob/master/images/flow.png)
+
+A [node.js](http://nodejs.org/) Node that detect and identify face on camera
+
+Requirements
+============
+
+* [node.js](http://nodejs.org/) -- tested against v11+
+
+* [node-red](http://nodered.org/)
+
+* Need [OpenEyes](http://open-eyes.it) hardware platform
+
+  ![OpenEyes-FaceIdentify-flow](https://github.com/nemax68/node-red-contrib-OpenEyes-GetButton/blob/master/images/open-eyes.png)
+
+Install
+=======
+
+```shell
+$ npm install openeyes-faceidentify
+```
+Values
+========
+
+![Values](https://github.com/nemax68/node-red-contrib-OpenEyes-Screen/blob/master/images/value.png)
+
+Examples
+========
+
+API
+===
---
+++ face/faceidentify.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2018 OPEN-EYES S.r.l.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+module.exports = function(RED) {
+  "use strict";
+
+	function faceid(config) {
+        RED.nodes.createNode(this,config);
+        var context = this.context();
+        this.swtag = config.swtag;
+        this.hwtag = config.hwtag;
+        this.urltag = config.urltag;
+        var node = this;
+
+        node.status({fill: "green", shape: "dot", text: node.swtag});
+
+        this.on('input', function(msg) {
+
+          const { exec } = require('child_process');
+
+          var cmd="oe_face_detect --swtag " + node.swtag + " --hwtag " + node.hwtag + " --url " + node.urltag;
+          console.log(cmd);
+          exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+              var outMsg = {payload: `${err}`};
+              node.send(outMsg);
+              return;
+            }
+            var outMsg = {payload: `${stdout}`};
+            node.send(outMsg);
+          });
+
+        });
+    }
+    RED.nodes.registerType("face identify",faceid);
+};
